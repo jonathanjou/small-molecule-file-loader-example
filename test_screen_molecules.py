1d7f2fb2c4fa98cc5f46f82fcdd69b28732a2906
jonathandjou <jonathan.jou@schrodinger.com>
2022-01-24 20:59:56 -0500

    Consistent variable names.

--- test_screen_molecules.py
+++ test_screen_molecules.py
@@ -13,28 +13,28 @@ def test_everything_in_one_go():
     s, sc, ds = screen_molecule('test_mae.mae', st, g, tpo, rt)
     assert s
     assert score == 8.13418631
-    assert structure.are_identical(docked_structure, reference_structure)
+    assert structure.are_identical(ds, r_st)
     s, sc, ds = screen_molecule('test_mae.mae.gz', st, g, tpo, rt)
     assert score == 8.13418631
-    assert structure.are_identical(docked_structure, reference_structure)
+    assert structure.are_identical(ds, r_st)
     s, sc, ds = screen_molecule('test_mae.maegz', st, g, tpo, rt)
     assert score == 8.13418631
-    assert structure.are_identical(docked_structure, reference_structure)
+    assert structure.are_identical(ds, r_st)
     s, sc, ds = screen_molecule('test_csv.csv', st, g, tpo, rt)
     assert score == 8.13418631
-    assert structure.are_identical(docked_structure, reference_structure)
+    assert structure.are_identical(ds, r_st)
     s, sc, ds = screen_molecule('test_csv.csv.gz', st, g, tpo, rt)
     assert score == 8.13418631
-    assert structure.are_identical(docked_structure, reference_structure)
+    assert structure.are_identical(ds, r_st)
     s, sc, ds = screen_molecule('test_csv.csvgz', st, g, tpo, rt)
     assert score == 8.13418631
-    assert structure.are_identical(docked_structure, reference_structure)
+    assert structure.are_identical(ds, r_st)
     s, sc, ds = screen_molecule('test_mae.mae', st, g, tpo, rt)
     assert score == 8.13418631
-    assert structure.are_identical(docked_structure, reference_structure)
+    assert structure.are_identical(ds, r_st)
     s, sc, ds = screen_molecule('test_mae.mae.gz', st, g, tpo, rt)
     assert score == 8.13418631
-    assert structure.are_identical(docked_structure, reference_structure)
+    assert structure.are_identical(ds, r_st)
     s, sc, ds = screen_molecule('test_mae.maegz', st, g, tpo, rt)
     assert score == 8.13418631
-    assert structure.are_identical(docked_structure, reference_structure)
+    assert structure.are_identical(ds, r_st)
